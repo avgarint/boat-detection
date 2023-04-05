398671a0c387711379bc7821a7b146e23b41194b
PilotAlex <66020831+AlexandreAero@users.noreply.github.com>
2023-04-05 17:57:14 +0200

    Updated quote style

--- BoatDetection/main.py
+++ BoatDetection/main.py
@@ -10,18 +10,18 @@ MAX_RGBA_VALUE = 255
 MAX_VALUE_RGB_COMP = 3
 
 def detect(img_path, save_path, remove_boats, export_as_mask):
-    """
+    '''
     str, str, bool, bool -> None
     Allows to detect the boats on a satellite imagery.
-    """
-    if "\\" in save_path:
-        save_path.replace("\\", "")
+    '''
+    if '\\' in save_path:
+        save_path.replace('\\', '')
 
     # Open image.
     img = Image.open(img_path)
 
-    # We keep in copy a version in "L".
-    img_luminance = img.convert("L")
+    # We keep in copy a version in 'L'.
+    img_luminance = img.convert('L')
     
     dom_color = get_dominant_color(img)
     brightness = get_brightness_image(img)
@@ -50,18 +50,18 @@ def detect(img_path, save_path, remove_boats, export_as_mask):
                     img.putpixel((x, y), DEBUG_COLOR_DETECTION)
 
     if export_as_mask:
-       export_detection_mask(img, save_path + "\Mask.PNG")
+       export_detection_mask(img, save_path + '\Mask.PNG')
 
     # We save the image.
-    img.save(save_path + "\Detection.PNG")
+    img.save(save_path + '\Detection.PNG')
 
 def export_detection_mask(img, save_path):
-    """
+    '''
     PIL.Image, str -> None
     Build an image of the detection mask from the pixels
     pixels detected as boats.
-    """
-    mask = Image.new("RGB", img.size)
+    '''
+    mask = Image.new('RGB', img.size)
     
     for x in range(img.size[0]):
         for y in range(img.size[1]):
@@ -73,10 +73,10 @@ def export_detection_mask(img, save_path):
     mask.save(save_path)
 
 def get_dominant_color(img):
-    """
+    '''
     Pil.Image -> tuple(r, g, b)
     Returns the dominant color of the image.
-    """
+    '''
     pixels = img.getdata()
 
     # We ingore the alpha channel.
@@ -93,11 +93,11 @@ def get_dominant_color(img):
     return tuple(dominant_color)
 
 def get_brightness_image(img):
-    """
+    '''
     Pil.Image -> float/int
     Returns the global brightness of the image.
-    """
-    copy_as_L = img.convert("L")
+    '''
+    copy_as_L = img.convert('L')
 
     histogram = copy_as_L.histogram()
     pixels = sum(histogram)
@@ -113,10 +113,10 @@ def get_brightness_image(img):
     return brightness / scale
 
 def get_optimal_tolerance(brightness):
-    """
+    '''
     float/int -> int
     Returns the most suitable tolerance for the image.
-    """
+    '''
     # The clearer the background (here the sea), the greater the tolerance should be
     # and reverse.
     if brightness * MAX_RGBA_VALUE > 65:
@@ -125,10 +125,10 @@ def get_optimal_tolerance(brightness):
     return 80
 
 def get_neighbour_coordinates(coordinates, pixel_distance):
-    """
+    '''
     Returns the coordinates of the 4 neighboring pixels of a pixel
     (top, bottom, right and left).
-    """ 
+    ''' 
     top_coord = (coordinates[0], coordinates[1] - pixel_distance)
     bottom_coord = (coordinates[0], coordinates[1] + pixel_distance)
     right_coord = (coordinates[0] + pixel_distance, coordinates[1])
@@ -138,8 +138,8 @@ def get_neighbour_coordinates(coordinates, pixel_distance):
 
 # Usage.
 detect(
-    img_path="Samples\Boat16.PNG",
-    save_path="Output",
+    img_path='Samples\Boat16.PNG',
+    save_path='Output',
     remove_boats=False,
     export_as_mask=True
 )
